58ea5a3fa46078b410a8543f16cf82a5684735f6
Kenia Vargas <kenia.vargascruz27@gmail.com>
2025-02-23 19:31:42 -0600

    feat: create GetUserByUsername Service

--- Backend/lambdas/get_user_by_username.js
+++ Backend/lambdas/get_user_by_username.js
@@ -1,5 +1,18 @@
+const { User } = require('../models/models');
+
 const getUserByUsername = async (req, res) => {
+    const { username } = req.params;
+    try {
+        const user = await User.findOne({
+            where: { username } 
+        });
 
+        if (!user) {
+            return res.status(404).json({ message: "User not found." });
+        }
+        res.status(200).json(user); 
+    } catch (error) {
+        res.status(500).json({ message: 'Error in service.' });
+    }
 };
-
 module.exports = getUserByUsername;
