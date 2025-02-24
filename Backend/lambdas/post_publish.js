b3d21a8ef7e7730cb933b46f50a75d2d0ecc0dca
Kenia Vargas <kenia.vargascruz27@gmail.com>
2025-02-23 19:41:45 -0600

    feat: create PostPublish Service

--- Backend/lambdas/post_publish.js
+++ Backend/lambdas/post_publish.js
@@ -1,6 +1,22 @@
+const { Publication } = require('../models/models');
 
 const postPublish = async (req, res) => {
-
+    try {
+        const { description, user_id } = req.body;
+    
+        if (!description || !user_id) {
+          return res.status(400).json({ message: 'Invalid params. Description and UserId are required.' });
+        }
+    
+        const createPublication = await Publication.create({
+          description,
+          user_id,
+        });
+    
+        return res.status(201).json(createPublication);
+      } catch (error) {
+        return res.status(500).json({ message: 'Error in service.' });
+      }
 };
 
 module.exports = postPublish;
