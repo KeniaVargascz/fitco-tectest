54fdfa06fd670b68b6d58deb6dbf5d30b36021b9
Kenia Vargas <kenia.vargascruz27@gmail.com>
2025-02-23 19:46:56 -0600

    feat: create PutReactionUser Service

--- Backend/lambdas/put_reaction_user.js
+++ Backend/lambdas/put_reaction_user.js
@@ -1,6 +1,32 @@
+const { Reactions } = require('../models/models');
 
 const putReactionUser = async (req, res) => {
+    try {
+        const { publication_id, user_id, value } = req.body;
 
+        const reaction = await Reactions.findOne({
+          where: {
+            user_id: user_id,
+            publication_id: publication_id
+          },
+          attributes: ['id', 'user_id', 'publication_id', 'value']
+
+        });
+    
+        if (reaction) {
+          await reaction.update({ value });
+        } else {
+          await Reactions.create({
+            user_id: user_id,
+            publication_id: publication_id,
+            value
+          });
+        }
+        return res.status(200).json({message: 'Reaction was added.'});
+
+      } catch (error) {
+        return res.status(500).json({message: 'Error in service.'});        
+      }
 };
 
 module.exports = putReactionUser;
