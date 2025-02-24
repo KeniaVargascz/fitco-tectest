c42de0d34751eb5055a7ad79b5d29c188216007a
Kenia Vargas <kenia.vargascruz27@gmail.com>
2025-02-24 00:03:27 -0600

    fix: delete no use libraries

--- Fronted/src/index.js
+++ Fronted/src/index.js
@@ -5,16 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from "./redux/store";
-import i18n from './i18n';
-import { I18nextProvider } from 'react-i18next';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
         <App />
-      </I18nextProvider>
     </Provider>
   </React.StrictMode>
 );
