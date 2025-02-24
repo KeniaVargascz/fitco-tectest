24122535f27158dc1ffc6ba5ee05fc551d3050e9
Kenia Vargas <kenia.vargascruz27@gmail.com>
2025-02-23 18:45:31 -0600

    feat: create server file

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,20 @@
-//
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const userRoutes = require("./routes/userRoutes");
+
+const PORT = 3005;
+
+app.use(express.json());
+
+app.use(cors({
+    origin: '*', 
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+}));
+
+app.use("/api", userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`ServerUp! http://localhost:${PORT}`);
+});
